b6cdfce7b41c5fe5318fd700ecb07b367d185c59
KOSMOGOR <mortalmortal2015@mail.ru>
2022-05-16 19:56:16 +0300

    create table clients

--- rzhd_db.py
+++ rzhd_db.py
@@ -2,12 +2,17 @@ import sqlite3
 DB_NAME = "railways.db"
 
 def db_init():
-	pass
-	#TODO2: add all tables
+	conn = sqlite3.connect(DB_NAME)
+	conn.execute("""CREATE TABLE clients(
+		client_id INTEGER PRIMARY KEY,
+		username TEXT,
+		password TEXT,
+		email TEXT
+	)""")
+	conn.commit()
 
 def db_check():
 	conn = sqlite3.connect(DB_NAME)
-	conn.execute()
 	cur = conn.cursor()
 	for i in cur.execute("SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"):
 		print(i)
