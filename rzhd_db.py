6d93cbef7fdfa8d16bb915a91248086f94219df2
KOSMOGOR <mortalmortal2015@mail.ru>
2022-05-16 20:01:03 +0300

    insert into client

--- rzhd_db.py
+++ rzhd_db.py
@@ -2,22 +2,26 @@ import sqlite3
 DB_NAME = "railways.db"
 
 def db_init():
-	conn = sqlite3.connect(DB_NAME)
-	conn.execute("""CREATE TABLE clients(
-		client_id INTEGER PRIMARY KEY,
-		username TEXT,
-		password TEXT,
-		email TEXT
-	)""")
-	conn.commit()
+    conn = sqlite3.connect(DB_NAME)
+    conn.execute("""CREATE TABLE clients(
+        client_id INTEGER PRIMARY KEY,
+        username TEXT,
+        password TEXT,
+        email TEXT
+    )""")
+    conn.execute("""INSERT INTO clients (username, password, email) VALUES
+    (ioann, dfgdgcxfgg, ioann@ioann.com),
+    (vasilly, dfgdfgdthf, vasilly@vasylly.com),
+    (pyotr, cgjhcrgdrg, pyotr@pyotr.com)""")
+    conn.commit()
 
 def db_check():
-	conn = sqlite3.connect(DB_NAME)
-	cur = conn.cursor()
-	for i in cur.execute("SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"):
-		print(i)
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    for i in cur.execute("SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"):
+        print(i)
 
 from os.path import exists
 if not exists(DB_NAME):
-	db_init()
+    db_init()
 db_check()
